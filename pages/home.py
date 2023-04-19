86a20f56cd26d97a92e51b496c074b9e33be1a64
nicktriplett <124808763+nicktriplett@users.noreply.github.com>
2023-04-19 00:07:44 -0500

    Updated home.py

--- pages/home.py
+++ pages/home.py
@@ -14,8 +14,35 @@ layout = html.Div(
         html.H1('MLB Comparison Dashboard (2022 Season)',className='text-center text-danger mt-3 mb-2 fs-1'),
         html.H2('A BSAN 406 Project Created by Nick Triplett',className='text-center text-dark mt-1 mb-2 fs-3'),
         html.P("Welcome to my Major League Baseball (MLB) Comparison Dashboard! This dashboard was created as part of many assigned projects through an analytics-oriented class at Southern Illinois University - Carbondale. Inside this dashboard, you'll discover multiple graphs and various statistical results coming from the 2022 MLB season. More information about each graph and results can be found on the other pages of this dashboard. Enjoy using this MLB dashboard!",className='text-center text-dark mb-0 mt-4 fs-6'),
-        html.Img(src=dash.get_asset_url('mlb-logo-image.png'),style={'width':'46%'},className='mb-0 mx-5'),
-        html.Img(src=dash.get_asset_url('mlb-word-image.png'),style={'width':'42%'},className='mb-0 ml-5'),
+        dbc.Row([
+            dbc.Col(
+                html.Img(
+                    src=dash.get_asset_url('mlb-logo-image.png'),
+                    style={
+                        'width':'100%',
+                        'vertical-align':'top',
+                        'object-fit':'contain'}
+                ),
+                width={'size': 5}
+            ),
+            dbc.Col(
+                html.Div(
+                    children=[
+                        html.Img(
+                            src=dash.get_asset_url('mlb-word-image2.png'),
+                            style={
+                                'width': '100%',
+                                'vertical-align':'top',
+                                'object-fit':'contain'}
+                        )
+                    ],
+                style={'margin-top': '45px'}
+                ),
+                width={'size': 5}
+            )
+            ],
+            justify='center'
+        ),
         html.Div(
             children=[
                 'Image Source: ',
@@ -24,7 +51,7 @@ layout = html.Div(
                     href='https://logolook.net/mlb-logo/',className='text-primary fs-4'
                 ),
         ],
-        className='text-dark text-center fs-4 mt-0'
+        className='text-dark text-center fs-4 mt-3'
         ),
         html.P('Note: This image was edited for the purpose of changing its appearance for this dashboard.',className='text-dark text-center fs-5')
     ]
